2da3bab142c89d685d37aadb782c02cfb9b5c82a
Ivan <ivanzhao8@gmail.com>
2020-10-03 01:49:37 -0400

    small changes to frotnend

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,20 +1,70 @@
-import React from 'react';
-import './HomePage.scss';
+import React from "react";
+import { TextField } from "@material-ui/core";
+import "./HomePage.scss";
 
 class HomePage extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    componentDidMount() {
-		// make api calls? if using sockets, maybe not, have to think about this
-	}
+  componentDidMount() {
+    // make api calls? if using sockets, maybe not, have to think about this
+  }
 
-    render() {
-        return (
-            <div>HomePage</div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            alignContent: "center",
+            fontSize: 64,
+          }}
+        >
+          Welcome to Lunchmeat!{" "}
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            alignContent: "center",
+            fontSize: 24,
+          }}
+        >
+          Lunchmeat is a way for you to meet other working people during your
+          lunch break!
+        </div>
+
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            alignContent: "center",
+            fontSize: 24,
+            marginTop: "50px",
+          }}
+        >
+          Let’s get started with your username!
+        </div>
+        <form
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            alignContent: "center",
+            fontSize: 24,
+            // marginTop: "50px",
+          }}
+        >
+          <TextField id="filled-basic" label="Username" variant="filled" />
+        </form>
+      </div>
+    );
+  }
 }
 
 export default HomePage;
