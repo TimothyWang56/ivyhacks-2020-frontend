929d7385e4b0441a8570816dc591b46b27cfe03a
TimothyWang56 <timothywang56@gmail.com>
2020-10-02 23:08:47 -0400

    embed basic video with dummy room link

--- src/Pages/VideoChatPage/VideoChatPage.js
+++ src/Pages/VideoChatPage/VideoChatPage.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import './VideoChatPage.scss';
+import DailyIFrame from '@daily-co/daily-js';
 
 class VideoChatPage extends React.Component {
     constructor(props) {
         super(props);
+        this.iframeRef = React.createRef();
     }
 
     componentDidMount() {
-		// make api calls? if using sockets, maybe not, have to think about this
+        this.callFrame = DailyIFrame.wrap(this.iframeRef.current);
+
+        // this is a test room
+        this.callFrame.join({ url: "https://ivyhacks-sit.daily.co/WGnRLqx7LQ1HqE9XtSvp" });
 	}
 
     render() {
         return (
-            <div>VideoChatPage</div>
+            <div className="video-page-container">
+                <iframe className="video-frame"
+                    title="video call iframe"
+                    ref={this.iframeRef}
+                    allow="camera; microphone; fullscreen"
+            />
+           </div>
         );
     }
 }
