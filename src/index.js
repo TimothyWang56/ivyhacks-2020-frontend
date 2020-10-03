7def57e1dc24e0a90bdbf9dfa85ecdca870daa24
SorinCho <sorincho@gmail.com>
2020-10-03 13:26:53 -0400

    Revert "Revert "loading page""
    This reverts commit de7c7b93ec7656a421937ef5afb3ac48e61f64cb.

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ import {
 
 import HomePage from './Pages/HomePage/HomePage';
 import VideoChatPage from './Pages/VideoChatPage/VideoChatPage';
-
 ReactDOM.render(
     <React.StrictMode>
         <Router>
