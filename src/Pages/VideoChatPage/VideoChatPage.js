5f91afa8c69c60aae6d793aeeb429f214c493b2a
TimothyWang56 <timothywang56@gmail.com>
2020-10-02 23:54:13 -0400

    basic video page format

--- src/Pages/VideoChatPage/VideoChatPage.js
+++ src/Pages/VideoChatPage/VideoChatPage.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './VideoChatPage.scss';
 import DailyIFrame from '@daily-co/daily-js';
+import TopicList from '../../Components/TopicList/TopicList';
 
 class VideoChatPage extends React.Component {
     constructor(props) {
         super(props);
         this.iframeRef = React.createRef();
+        this.state = {
+            time: 0,
+        }
     }
 
     componentDidMount() {
@@ -13,16 +17,36 @@ class VideoChatPage extends React.Component {
 
         // this is a test room
         this.callFrame.join({ url: "https://ivyhacks-sit.daily.co/WGnRLqx7LQ1HqE9XtSvp" });
-	}
+
+        this.setState({ time: 0 });
+        this.interval = setInterval(() => this.setState({ time: this.state.time + 1 }), 1000);
+    }
+    
+    formatTime(secs) {
+        let hours = Math.floor(secs / 3600);
+        let minutes = Math.floor(secs / 60) % 60;
+        let seconds = secs % 60;
+        return [hours, minutes, seconds]
+            .map(v => ('' + v).padStart(2, '0'))
+            .filter((v,i) => v !== '00' || i > 0)
+            .join(':');
+      }
 
     render() {
         return (
             <div className="video-page-container">
+                <div className="topic-list">
+                    <TopicList topics={["Interests", "Hobbies", "Games", "Food", "Work", "Travel"]}/>
+                </div>
+                <div className="leave-button" onClick={() => console.log("leave pressed")}>LEAVE</div>
                 <iframe className="video-frame"
                     title="video call iframe"
                     ref={this.iframeRef}
                     allow="camera; microphone; fullscreen"
-            />
+                />
+                <div className="video-timer">
+                    {this.formatTime(this.state.time)}
+                </div>
            </div>
         );
     }
